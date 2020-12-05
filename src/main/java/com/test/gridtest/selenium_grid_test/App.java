3fed6363f8dd25091d1705253597d2a9204c65b4
waheed khan <wahid.khan74@gmail.com>
2020-12-05 16:23:47 +0000

    selenium grid test

---
+++ src/main/java/com/test/gridtest/selenium_grid_test/App.java
@@ -0,0 +1,13 @@
+package com.test.gridtest.selenium_grid_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/test/gridtest/selenium_grid_test/GridTest.java
@@ -0,0 +1,41 @@
+package com.test.gridtest.selenium_grid_test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GridTest {
+
+  WebDriver driver ;
+  String siteUrl,nodeUrl;
+  
+  @BeforeTest
+  public void setUp() throws MalformedURLException {
+	  siteUrl = "https://www.google.com/";
+	  nodeUrl = "http://172.17.0.1:4444/wd/hub";
+	  
+	  DesiredCapabilities cap = new DesiredCapabilities();
+	  cap.setBrowserName("chrome");
+	  cap.setPlatform(Platform.LINUX);	  
+	  driver = new RemoteWebDriver(new URL(nodeUrl),cap);
+  }
+  
+  @Test
+  public void loadGoogleHomePageTest() {
+	  driver.get(siteUrl);
+	  Assert.assertEquals("Google", driver.getTitle());
+  }
+  
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+}
